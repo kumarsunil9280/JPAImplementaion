6ddc9f63f292acb867ee32c727c03ec0c9ef7193
Sunil Kumar <sunilkumar.epri@gmail.com>
2026-01-14 09:53:35 +0530

    security

---
+++ src/main/java/com/example/docker/config/JwtUtil.java
@@ -0,0 +1,30 @@
+package com.example.docker.config;
+
+import java.util.Date;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET_KEY = "secret";
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
+
